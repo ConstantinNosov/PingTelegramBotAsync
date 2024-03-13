d831f0080241a1de79e948da1b4e132ed78d4574
ConstantinNosov <constantinnosov258@gmail.com>
2024-03-13 23:03:14 +0600

    beta

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import asyncio
 from config import bot_token, chat_id
 from async_bot_lib import AsyncTelegramBot
-from pingtester_lib import AsyncPingTester
+from async_pingtester_lib import AsyncPingTester
 
 async def daily_notification(bot):
     """Отправляет уведомление о работоспособности бота раз в сутки."""
@@ -10,7 +10,6 @@ async def daily_notification(bot):
         await bot.send_message("Привет, бот работает !")
         # Ожидание 24 часа (86400 секунд)
         await asyncio.sleep(86400)
-
 
 async def ip_test(bot):
     """Отправляет уведомление о недоступных хостах."""
@@ -22,12 +21,12 @@ async def ip_test(bot):
 
 async def main():
     bot = AsyncTelegramBot(chat_id, bot_token)
-    # Запуск задачи отправки уведомления о работоспособности
+    # Запуск задачи отправки уведомлений 
     asyncio.create_task(daily_notification(bot))
     while True:
         await ip_test(bot)
         # Проверка каждые 5 минут
-        await asyncio.sleep(5)  # Изменено с 5 секунд для реалистичности
+        await asyncio.sleep(900)  # Интервал опроса хостов в секундах
 
 if __name__ == "__main__":
     asyncio.run(main())
